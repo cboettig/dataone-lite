4bffecb5ab9b536decc48d9bcb1efe9b88ae6348
Carl Boettiger <cboettig@gmail.com>
2015-01-31 13:40:32 -0800

    Update d1_upload.R

--- tests/testthat/d1_upload.R
+++ tests/testthat/d1_upload.R
@@ -5,6 +5,8 @@ test_that("We can upload data to DataONE",
 {
   data(nodes)
   f <- system.file("examples", "example-eml.xml", package="dataonelite")
+  # cert file must be downloaded manually locally. We use a location within the package 
+  # to simplify testing from within containers, which won't share `/tmp` with the host.
   cert <- system.file("tmp", "x509up_u1000", package="dataonelite")
   
   ## We can only test this function if we have a certificate
